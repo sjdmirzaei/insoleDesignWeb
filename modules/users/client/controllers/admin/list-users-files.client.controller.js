56adeb4592c8d2bf00d0bebb4bcb055074cc9296
MNR85 <milad.n.r.85@gmail.com>
2018-11-18 15:10:04 +0330

    add forgotten files!!

---
+++ modules/users/client/controllers/admin/list-users-files.client.controller.js
@@ -0,0 +1,66 @@
+(function () {
+    'use strict';
+
+  angular
+    .module('users.admin')
+    .controller('UserFileListController', UserFileListController);
+
+  UserFileListController.$inject = ['$scope', '$filter', '$state', '$window', 'AdminService','Notification'];
+
+  function UserFileListController($scope, $filter, $state, $window, AdminService,Notification) {
+    var vm = this;
+    vm.buildPager = buildPager;
+    vm.figureOutItemsToDisplay = figureOutItemsToDisplay;
+    vm.pageChanged = pageChanged;
+    vm.removeComplete = removeComplete;
+    refreshData();
+    function refreshData(){
+      console.log('refresh list');
+      AdminService.usersFiles(function (data) {
+        vm.users = data;
+        vm.buildPager();
+      });
+    }
+
+
+    function removeComplete(pathName){
+      if ($window.confirm('Are you sure you want to delete?'))
+      AdminService.completeDelete(pathName).$promise.then(function (data) {
+        // vm.error = data.message;
+        // Notification.success({message: data.message});
+        if (data.msgtype == 'error') {
+          Notification.error({message: data.message});
+        } else {
+          Notification.success({message: data.message});
+        }
+        refreshData();
+        $state.go('admin.usersFiles');
+      }).catch(function(err){
+        Notification.error({message: 'خطا در پردازش'});
+        refreshData();
+        $state.go('admin.usersFiles');
+      });
+      //refreshData();
+    }
+    function buildPager() {
+      vm.pagedItems = [];
+      vm.itemsPerPage = 15;
+      vm.currentPage = 1;
+      vm.figureOutItemsToDisplay();
+    }
+
+    function figureOutItemsToDisplay() {
+      vm.filteredItems = $filter('filter')(vm.users, {
+        $: vm.search
+      });
+      vm.filterLength = vm.filteredItems.length;
+      var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+      var end = begin + vm.itemsPerPage;
+      vm.pagedItems = vm.filteredItems.slice(begin, end);
+    }
+
+    function pageChanged() {
+      vm.figureOutItemsToDisplay();
+    }
+  }
+}());
